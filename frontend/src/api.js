4a666b698dba89dc462d450b861d066c1044f1f2
maithrisharma <maithribharadwaj3@gmail.com>
2025-12-11 19:40:42 +0100

    base url fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 const BASE_URL =
-  (import.meta.env.VITE_API_BASE_URL && import.meta.env.VITE_API_BASE_URL + "/api")
-  || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 /**
  * Helper for GET requests
