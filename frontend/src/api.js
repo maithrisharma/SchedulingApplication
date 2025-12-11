d666b1b57ea7ce90bb5bce00378e2ebb367249a4
maithrisharma <maithribharadwaj3@gmail.com>
2025-12-11 19:31:24 +0100

    base url change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+const BASE_URL =
+  (import.meta.env.VITE_API_BASE_URL && import.meta.env.VITE_API_BASE_URL + "/api")
+  || "http://localhost:5000/api";
 
 /**
  * Helper for GET requests
