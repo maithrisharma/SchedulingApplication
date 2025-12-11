65e1487b96c26aed68b058726745136c17fa581a
maithrisharma <maithribharadwaj3@gmail.com>
2025-12-11 18:54:51 +0100

    wsgi file

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,6 +2,6 @@ from app import create_app
 
 app = create_app()
 
-# Optional: only used if someone runs `python wsgi.py`
+#  only used if someone runs `python wsgi.py`
 if __name__ == "__main__":
     app.run()
